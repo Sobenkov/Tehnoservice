90f85ad30b6d4e8d4854609a214226295eaf38bd
Teranion <Sobenkov95@yandex.ru>
2019-12-20 19:57:20 +0300

    Tehnoservice

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,3 +1,29 @@
+$(document).ready(function() {
+  var menuBtn = $('.top-nav-btn');
+  var serviceBtnS = $('#iconSer');
+  var serviceBtnC = $('#iconCat');
+  var menu = $('.top-nav-menu');
+  var serviceMenuS = $('.main-barSer');
+  var serviceMenuC = $('.main-barCat');
+
+  menuBtn.on('click',  function(event) {
+    event.preventDefault();
+    menu.toggleClass('top-nav-menu_active');
+  });
+
+  serviceBtnS.on('click',  function(event) {
+    event.preventDefault();
+    serviceBtnS.toggleClass('icon-up-open');
+    serviceMenuS.toggleClass('main-barSer_active');
+  });
+
+  serviceBtnC.on('click',  function(event) {
+    event.preventDefault();
+    serviceBtnC.toggleClass('icon-up-open');
+    serviceMenuC.toggleClass('main-barCat_active');
+  });
+
+});
 
 $('#sliderHead').slick({
   dots: true,
@@ -16,4 +42,19 @@ $('#sliderProject').slick({
   slidesToScroll: 1,
   nextArrow: '<i class="icon-right-open-1" aria-hidden="true"></i>',
   prevArrow: '<i class="icon-left-open-1" aria-hidden="true"></i>',
+  responsive: [
+          {
+          breakpoint: 600,
+          settings:{
+            slidesToShow: 1
+          }
+          },
+
+          {
+          breakpoint: 1024,
+          settings:{
+            slidesToShow: 2
+          }
+          },
+              ]
 });
